e00732f575affcb902e3b513221b6c1c3c11c4d4
ThilenSavignon <thilen.savignon@gmail.com>
2025-03-05 14:16:57 +0100

    step-6: CPU Interrupts (double fault handler)

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -4,10 +4,16 @@ use crate::println;
 
 use lazy_static::lazy_static;
 
+use crate::gdt;
+
 lazy_static! {
 	static ref IDT: InterruptDescriptorTable = {
 		let mut idt = InterruptDescriptorTable::new();
 		idt.breakpoint.set_handler_fn(breakpoint_handler);
+		unsafe {
+			idt.double_fault.set_handler_fn(double_fault_handler)
+				.set_stack_index(gdt::DOUBLE_FLOAT_IST_INDEX);
+		}
 		idt
 	};
 }
@@ -21,6 +27,12 @@ extern "x86-interrupt" fn breakpoint_handler(
 ) {
 	println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
+
+extern "x86-interrupt" fn double_fault_handler(
+	stack_frame: InterruptStackFrame, _error_code: u64
+ ) -> ! {
+	panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+ }
 
 #[test_case]
 fn test_breakpoint_exception() {
--- src/lib.rs
+++ src/lib.rs
@@ -72,7 +72,10 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 pub mod interrupts;
+pub mod gdt;
 
 pub fn init() {
+    gdt::init();
     interrupts::init_idt();
 }
+
