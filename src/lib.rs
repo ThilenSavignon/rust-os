246654e460c7d52d80d767af7f9f35fec0d0ba4f
ThilenSavignon <thilen.savignon@gmail.com>
2025-03-05 16:01:56 +0100

    step-7: Hardware Interrupts (Timer/Keyboard)

--- src/lib.rs
+++ src/lib.rs
@@ -37,7 +37,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     serial_println!("[failed]\n");
     serial_println!("Error: {}\n", info);
     exit_qemu(QemuExitCode::Failed);
-    loop {}
+    hlt_loop();
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -62,7 +62,7 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 pub extern "C" fn _start() -> ! {
     init();
     test_main();
-    loop {}
+    hlt_loop();
 }
 
 #[cfg(test)]
@@ -77,5 +77,12 @@ pub mod gdt;
 pub fn init() {
     gdt::init();
     interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
 }
 
+pub fn hlt_loop() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,14 @@ pub extern "C" fn _start() -> ! {
 
     os::init();
 
-    fn stack_overflow() {
-        stack_overflow();
-    }
-
-    stack_overflow();
+   
 
     #[cfg(test)]
     test_main();
 
 
     println!("It did not crash!");
-    loop {}
+    os::hlt_loop();
 }
 
 /// This function is called on panic.
@@ -32,7 +28,7 @@ pub extern "C" fn _start() -> ! {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
-    loop {}
+    os::hlt_loop();
 }
 
 #[cfg(test)]
