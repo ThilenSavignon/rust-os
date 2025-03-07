ebb0f50d6dbe3f06cc91e2d6d2ade22a1bfb7d48
ThilenSavignon <thilen.savignon@gmail.com>
2025-03-07 16:20:08 +0100

    step-9: Paging Implementation

--- src/lib.rs
+++ src/lib.rs
@@ -56,10 +56,16 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     }
 }
 
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
+
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
 /// Entry point for `cargo xtest`
 #[cfg(test)]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
+
     init();
     test_main();
     hlt_loop();
@@ -86,3 +92,5 @@ pub fn hlt_loop() -> ! {
         x86_64::instructions::hlt();
     }
 }
+
+pub mod memory;
